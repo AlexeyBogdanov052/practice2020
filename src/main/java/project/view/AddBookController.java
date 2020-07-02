4c1fdbf1803818c599502d2484e4477dfb621969
AlexeyBogdanov052 <aleksey.bogdanov.99@mail.ru>
2020-07-02 11:38:37 +0300

    Commit 5

--- src/main/java/project/view/AddBookController.java
+++ src/main/java/project/view/AddBookController.java
@@ -1,15 +1,21 @@
 package project.view;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import project.dao.ListRep;
 import project.domain.ListEntity;
 
 @Controller
 public class AddBookController {
+    @Autowired
+    private ListRep listRep;
 
     @RequestMapping(value={"/addlist"}, method=RequestMethod.GET)
     public String listForm(Model model) {
@@ -19,7 +25,14 @@ public class AddBookController {
 
     @RequestMapping(value={"/addlist"}, method=RequestMethod.POST)
     public String listSubmit(@ModelAttribute ListEntity addlist, Model model) {
-        model.addAttribute("addlist", addlist);
-        return "result";
+        if (StringUtils.hasText(addlist.getName())){
+            ListEntity result = listRep.save(new ListEntity(addlist.getName()));
+            Long id = result.getId();
+
+            //return "redirect:/list/" + id;
+        }
+
+        return "redirect:/list";
     }
+
 }
